21298e0840820f43f7bad72d32cc70651c92b2e6
wanshw <wansw1994@163.com>
2019-04-22 18:12:21 +0800

    feat:爬取掘金自己够买的小册数据

--- README.md
+++ README.md
@@ -5,4 +5,5 @@
 - mdformat:给MD文档生成标签头
 - html2pdf:html转pdf
 - juejin:爬取掘金自己够买小册数据
-- qrcode;对输入的文本生成二维码
+- qrcode:对输入的文本生成二维码
+- md2html:md转html
---
+++ format/md2html.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import markdown
+
+markdown.markdownFromFile(input='Filter.md', output='Filter.html', encoding='utf-8')
